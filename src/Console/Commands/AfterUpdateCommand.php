68f4c1d76330f8eff1fb84d9fc77514d2f85f737
rutatiina <rutatiina@ruginem.org>
2022-10-08 16:41:51 +0300

    Cancel transaction init

--- src/Console/Commands/AfterUpdateCommand.php
+++ src/Console/Commands/AfterUpdateCommand.php
@@ -5,7 +5,27 @@ namespace Rutatiina\Qbuks\Console\Commands;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Log;
 use Rutatiina\Banking\Models\Transaction;
+use Rutatiina\Bill\Models\RecurringBill;
+use Rutatiina\CreditNote\Models\CreditNote;
+use Rutatiina\DebitNote\Models\DebitNote;
+use Rutatiina\Estimate\Models\Estimate;
+use Rutatiina\Expense\Models\Expense;
+use Rutatiina\Expense\Models\RecurringExpense;
 use Rutatiina\FinancialAccounting\Models\Account;
+use Rutatiina\GoodsDelivered\Models\GoodsDelivered;
+use Rutatiina\GoodsIssued\Models\GoodsIssued;
+use Rutatiina\GoodsReceived\Models\GoodsReceived;
+use Rutatiina\GoodsReturned\Models\GoodsReturned;
+use Rutatiina\Invoice\Models\Invoice;
+use Rutatiina\Invoice\Models\RecurringInvoice;
+use Rutatiina\PaymentMade\Models\PaymentMade;
+use Rutatiina\PaymentReceived\Models\PaymentReceived;
+use Rutatiina\PettyCash\Models\PettyCashEntry;
+use Rutatiina\POS\Models\POSOrder;
+use Rutatiina\PurchaseOrder\Models\PurchaseOrder;
+use Rutatiina\RetainerInvoice\Models\RetainerInvoice;
+use Rutatiina\Sales\Models\Sales;
+use Rutatiina\SalesOrder\Models\SalesOrder;
 
 class AfterUpdateCommand extends Command
 {
@@ -74,6 +94,56 @@ class AfterUpdateCommand extends Command
                 ->update([
                     'bank_account_financial_account_code' => $bankAccount->financial_account_code
                 ]);
+        }
+        $this->info("* Complete.");
+
+        //Find deleted transactions and mark them as canceled
+        $this->info("* Find deleted transactions and mark them as canceled.");
+        $tables = [
+
+            //banking
+            'rg_banking_transactions' => Transaction::class,
+
+            //pos
+            'rg_pos_orders' => POSOrder::class,
+
+            //sales
+            'rg_sales' => Sales::class,
+            'rg_estimates' => Estimate::class,
+            'rg_retainer_invoices' => RetainerInvoice::class,
+            'rg_sales_orders' => SalesOrder::class,
+            'rg_invoices' => Invoice::class,
+            'rg_payments_received' => PaymentReceived::class,
+            'rg_recurring_invoices' => RecurringInvoice::class,
+            'rg_credit_notes' => CreditNote::class,
+
+            //purchases & expenses
+            'rg_petty_cash_entries' => PettyCashEntry::class,
+            'rg_expenses' => Expense::class,
+            'rg_expense_recurring_expenses' =>RecurringExpense::class,
+            'rg_purchase_orders' => PurchaseOrder::class,
+            'rg_bills' => Bill::class,
+            'rg_payments_made' => PaymentMade::class,
+            'rg_bill_recurring_bills' => RecurringBill::class,
+            'rg_debit_notes' =>DebitNote::class,
+
+            //inventory
+            'rg_goods_received' =>GoodsReceived::class,
+            'rg_goods_delivered' => GoodsDelivered::class,
+            'rg_goods_issued' => GoodsIssued::class,
+            'rg_goods_returned' => GoodsReturned::class,
+			
+		];
+
+        foreach ($tables as $t => $model) 
+        {
+            $model::withTrashed()
+                ->whereNotNull('deleted_at')
+                ->update(['canceled' => 1]);
+            
+            $model::withTrashed()
+                ->whereNotNull('deleted_at')
+                ->restore();  
         }
         $this->info("* Complete.");
 
